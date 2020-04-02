e6582ec70c7d189fce2a7b72de471a0d59e2b1d2
David F <d_fi@gmx.de>
2020-04-02 20:49:08 +0200

    deleted unnecessary file
    Former-commit-id: c87ad6c1f8f0d3d3417b10a44b7a01c4fc74e43c

--- anki_scripts/new.py
+++
@@ -1,9 +0,0 @@
-import re
-
-
-search_term = "casa"
-
-with open(f"{search_term}/source.txt") as file:
-    # thumbnails = [line.split()[-2] for line in file]
-    thumbnails = [line.split("\t")[0] for line in file]
-    print(thumbnails)
