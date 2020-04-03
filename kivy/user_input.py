051699a3e7dfaec3b39319b4f24281bc3fc2f05b
David F <d_fi@gmx.de>
2020-04-03 19:17:24 +0200

    connected input fields with variables
    Former-commit-id: 72426c72279e1c0bb74073ed3a4bc8a19b13bda1

--- kivy/user_input.py
+++ kivy/user_input.py
@@ -1,9 +1,33 @@
 from kivy.lang import Builder
+from kivy.properties import StringProperty, ListProperty, BooleanProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivymd.app import MDApp
+from kivymd.uix.chip import MDChip, MDChooseChip
+
+
+class MyChip(MDChip):
+    def is_checked(self,*args):
+        return self.color == self.selected_chip_color
+
+class MyChooseChip(MDChooseChip):
+    def choice(self):
+        return [chip.label for chip in self.children if chip.is_checked()]
 
 class WordProperties(BoxLayout):
-    pass
+    search_term = StringProperty("casa")
+    gender = StringProperty("f")
+    translation = StringProperty("Haus")
+    synonyms = ListProperty(["domicilio","habitacion"])
+    examples = ListProperty(["Estoy em casa.","A casa e grande."])
+
+    def print_all(self):
+        print(f""""
+Search Term: {self.search_term}
+Gender: {self.gender}
+Translation: {self.translation}
+Synonyms: {self.synonyms}
+Examples: {self.examples}
+""")
 
 Builder.load_file("user_input.kv")
 
