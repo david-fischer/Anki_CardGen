eb43c86a563989aaa62abadec26c241423385125
David F <d_fi@gmx.de>
2020-04-06 15:37:15 +0200

    layout
    Former-commit-id: 26378ec3497b1d04b02446fb956e1a6a766f0876

--- kivy/user_input.py
+++ kivy/user_input.py
@@ -11,24 +11,26 @@ Builder.load_file("user_input.kv")
 class WordProperties(BoxLayout):
     search_term = StringProperty("")
     gender = StringProperty("")
-    translation = StringProperty("")
+    translations = ListProperty([])
     synonyms = ListProperty([])
     examples = ListProperty([])
+    display_limit = 5
 
     def refresh_data(self):
         word = MDApp.get_running_app().word
-        self.synonyms = word.synonyms
-        self.examples = word.examples
-        self.translation = word.translated
-        self.gender = word.gender
-
-    def post_init(self, ):
+        self.ids.translation_chips.clear_widgets()
         self.ids.synonym_chips.clear_widgets()
+        self.ids.antonym_chips.clear_widgets()
         self.ids.example_chips.clear_widgets()
-        for syn in self.synonyms:
+        for trans in word.translations[:self.display_limit]:
+            self.ids.translation_chips.add_widget(MyChip(label=trans, icon='', check=True))
+        for syn in word.synonyms[:self.display_limit]:
             self.ids.synonym_chips.add_widget(MyChip(label=syn, icon='', check=True))
-        for ex in self.examples:
+        for ant in word.antonyms[:self.display_limit]:
+            self.ids.antonym_chips.add_widget(MyChip(label=ant, icon='', check=True))
+        for ex in word.examples[:self.display_limit]:
             self.ids.example_chips.add_widget(MyChip(label=ex, icon='', check=True))
+        self.ids.explanation_dropdown.items=["Explanation"]+word.explanations
 
     def print_all(self):
         print(f""""
