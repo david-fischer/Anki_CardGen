40ae8cc9171c155d81fc7f9a0361e58272fb6b5c
David Fischer <d_fi@gmx.de>
2020-06-04 22:53:40 +0200

    change: confirmation button triggers confirm_choice() call
    Former-commit-id: f307399b8875ee9f39192165809546be36113c51 [formerly 6cced018c93881372cef4a447ed12d8ddcc72625]
    Former-commit-id: de262a9ccabfb7be5c0e686f8e4cbef7efe2289e

--- my_kivy/single_word.py
+++ my_kivy/single_word.py
@@ -1,16 +1,22 @@
 import os
+from datetime import datetime
 
 import certifi
 from kivy.network.urlrequest import UrlRequest
 from kivy.properties import StringProperty
 from kivy.uix.floatlayout import FloatLayout
 from kivymd.app import MDApp
+from kivymd.toast import toast
 from kivymd.uix.tab import MDTabsBase
 
 from my_kivy.mychooser import MyCheckImageGrid
-from utils import selection_helper, widget_by_id
+from utils import save_dict_to_csv, selection_helper, widget_by_id
 
 os.environ['SSL_CERT_FILE'] = certifi.where()
+
+
+def now_string():
+    return str(datetime.now()).split(".")[0].replace(" ", "_")
 
 
 def get_selection_dict():
@@ -21,14 +27,14 @@ def get_selection_dict():
         img_url = widget_by_id("/screen_single_word/image_tab/image_grid/").get_checked(property="source")[
             0].replace("http:", "https:")
         print(img_url)
-        UrlRequest(img_url, file_path=f"{base_path}.jpg",
-                   on_success=lambda *args: print("Finished downloading image."))
+        r_i = UrlRequest(img_url, file_path=f"{base_path}.jpg",
+                         on_success=lambda *args: print("Finished downloading image."))
     except IndexError:
         # TODO: change to a popup
         print("Error with image download. Try different Image instead.")
     audio_url = word.audio_url
-    UrlRequest(audio_url, file_path=f"{base_path}.mp3",
-               on_success=lambda *args: print("Finished downloading audio."))
+    r_a = UrlRequest(audio_url, file_path=f"{base_path}.mp3",
+                     on_success=lambda *args: print("Finished downloading audio."))
     selections = {
         "translation_chips": ["text"],
         "synonym_chips":     ["text_orig", "text_trans"],
@@ -40,19 +46,21 @@ def get_selection_dict():
     for key, props in selections.items():
         new_key = key.split("_")[0]
         out[new_key] = selection_helper(word_prop, id=key, props=props)
-    print(out)
+    # print(out)
     # TODO: Deal with the case that either audio or image is not downloaded
+    r_i.wait()
+    r_a.wait()
     return {
         'Word':               word.search_term,
         'Translation':        ", ".join(out["translation"]),
-        'Synonym':            out["synonym"][0],
+        'Synonym':            out["synonym"][0] if out["synonym"] else "",
         'Image':              f'<img src="{base_path}.jpg">',
-        'Explanation':        out["explanation"],
+        'Explanation':        out["explanation"][0],
         'ExampleTranslation': out["example"][1],
         'Example':            out["example"][0],
         'ConjugationTable':   "",
         'Audio':              f'[sound:{base_path}.mp3]',
-        'Antonym':            out["antonym"],
+        'Antonym':            out["antonym"][0] if out["antonym"] else "",
         'AdditionalInfo':     str(word.add_info_dict),
         'media_files':        [f"{base_path}.{ext}" for ext in ["jpg", "mp3"]],
     }
@@ -70,3 +78,13 @@ class ImageSearchResultGrid(MyCheckImageGrid):
         word = MDApp.get_running_app().word
         paths = word.image_urls if keywords is None else word.request_img_urls(keywords=keywords)
         self.element_dicts = [{"source": url} for url in paths]
+
+
+def confirm_choice():
+    result_dict = get_selection_dict()
+    print(result_dict)
+    toast(f'Added card for "{result_dict["Word"]}" to Deck.', duration=3)
+    save_dict_to_csv(result_dict, "out.csv")
+    MDApp.get_running_app().anki.add_card(**result_dict)
+    MDApp.get_running_app().anki.write_apkg(now_string() + ".apgk")
+    widget_by_id("/single_word_screen//")
