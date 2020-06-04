32afa46be3a4474ecbcb661204c7c24f548ff9e4
David Fischer <d_fi@gmx.de>
2020-06-04 22:43:23 +0200

    add: dialog to choose which replacements to accept
    Former-commit-id: 35500361f8f01f13e6828252cff0eac449fdc93d

--- my_kivy/kindle_import.py
+++ my_kivy/kindle_import.py
@@ -4,6 +4,7 @@ from kivy.clock import Clock
 from kivy.properties import BooleanProperty, ListProperty, ObjectProperty
 from kivy.uix.stacklayout import StackLayout
 from kivymd.app import MDApp
+from kivymd.uix.button import MDFlatButton
 from kivymd.uix.filemanager import MDFileManager
 
 from utils import clean_up, word_list_from_kindle
@@ -25,33 +26,50 @@ class EditKindleImport(StackLayout):
 
     def load_kindle(self, path):
         self.exit_manager()
-        words = clean_up(word_list_from_kindle(path),lemmatize=False)
+        words = clean_up(word_list_from_kindle(path), lemmatize=False)
         lemmas = clean_up(words)
         suggested_replacements = [f"{old} -> {new}" for old, new in zip(words, lemmas) if old != new]
-        unchanged_words = [word for word,lemma in zip(words,lemmas) if word==lemma]
+        unchanged_words = [word for word, lemma in zip(words, lemmas) if word == lemma]
         error_words = []
         event = Clock.schedule_once(partial(self.batch_request_words, words=unchanged_words, error_words=error_words))
-        event2 = Clock.schedule_once(partial(self.choose_replacements, replacements=suggested_replacements))
+        event2 = Clock.schedule_once(partial(self.choose_replacements, replacements=suggested_replacements,
+                                             error_words=error_words))
 
-    def batch_request_words(self,dt,words,error_words,*args):
+    def batch_request_words(self, dt, words, error_words, *args):
         for word in words:
             try:
                 MDApp.get_running_app().word.search(word)
-            except NoMatchError:
+            except (NoMatchError, KeyError):
                 error_words.append(word)
         print(error_words)
 
-    # def choose_replacements(self,dt,replacements,*args):
-    #     words = []
-    #     def button_function(obj):
-    #         words.append(obj.text.split(" ")[-1])
-    #     MDApp.get_running_app().show_dialog(
-    #         message="Some words are not in their dictionary form. The following replacements are suggested:",
-    #         options=replacements,
-    #         callback=print,
-    #         button_function=lambda x: x.parent.remove_widget(x)
-    #     )
+    def choose_replacements(self, dt, replacements, error_words):
+        words = []
 
+        def item_function(obj):
+            words.append(obj.text.split(" ")[-1])
+            obj.parent.remove_widget(obj)
+
+        def button_function(obj):
+            for item in MDApp.get_running_app().dialog.items:
+                words.append(item.text.split(" ")[0])
+                obj.parent.remove_widget(item)
+            MDApp.get_running_app().dialog.dismiss()
+            Clock.schedule_once(partial(self.batch_request_words, words=words, error_words=error_words))
+
+        ok_button = MDFlatButton(
+            text="OK",
+            text_color=MDApp.get_running_app().theme_cls.primary_color,
+            on_press=button_function
+        )
+
+        MDApp.get_running_app().show_dialog(
+            message="Some words are not in their dictionary form. The following replacements are suggested:",
+            options=replacements,
+            callback=print,
+            item_function=item_function,
+            buttons=[ok_button]
+        )
 
     def file_manager_open(self):
         self.file_manager.show('./test/test_data/')  # output manager to the screen
