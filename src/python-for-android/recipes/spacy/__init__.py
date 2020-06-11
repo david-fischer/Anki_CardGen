39abadf86b2201cf4b111cdf6ada5f951979b945
David Fischer <d_fi@gmx.de>
2020-06-12 00:17:26 +0200

    partial fix: dependencies in requirements.txt and buildozer.spec
    Additionally, added recipies for
    cataluogue, cymem, murmurhash, preshed, spacy, thinc, blis
    THE BLIS RECIPE IS NOT WORKING YET.
    
    The problem is that the android arch we compile for is 32bit and spacy and its dependencies
    contain precompiled elements that only work on 64bit. The recipies should enable
    the system to build everything from source, but the blis-recipe throws an error.
    
    
    Former-commit-id: d713616cfe17aee60033db6b2372fdc6a124a51e

---
+++ src/python-for-android/recipes/spacy/__init__.py
@@ -0,0 +1,17 @@
+from pythonforandroid.recipe import CppCompiledComponentsPythonRecipe
+
+
+class SpacyRecipe(CppCompiledComponentsPythonRecipe):
+    version = '2.2.4'
+    url = 'https://github.com/explosion/spaCy/archive/v{version}.tar.gz'
+    call_hostpython_via_targetpython = False
+
+    # depends = []
+    depends = ["preshed", "thinc", "blis", "tqdm", "setuptools", "murmurhash", "numpy", "wasabi", "plac",
+               "catalogue", "cymem", "requests", "srsly"]
+
+
+    site_packages_name = 'spacy'
+
+
+recipe = SpacyRecipe()
