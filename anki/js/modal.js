81d458dfa5cfd10e6b7fc9e07441c957c8e48374
David Fischer <d_fi@gmx.de>
2020-07-03 16:43:01 +0200

    fix: modal now works also on mobile device
    Former-commit-id: dbc1146f42a764ace1a4d24e6e287b1feec7a10b

--- anki/js/modal.js
+++ anki/js/modal.js
@@ -3,25 +3,25 @@ const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
 
 openModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
+  button.onclick = () => {
     const modal = document.querySelector(button.dataset.modalTarget)
     openModal(modal)
+  }
   })
-})
 
-overlay.addEventListener('click', () => {
+overlay.onclick=() => {
   const modals = document.querySelectorAll('.modal.active')
   modals.forEach(modal => {
     closeModal(modal)
-  })
-})
+  })}
+
 
 closeModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
+  button.onclick = () => {
     const modal = button.closest('.modal')
     closeModal(modal)
-  })
-})
+  }})
+
 
 function openModal(modal) {
   if (modal == null) return
