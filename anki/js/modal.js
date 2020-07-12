bb0a2eebb3b67b8a7ddf04a7f20cb39e224b3094
David Fischer <d_fi@gmx.de>
2020-07-12 18:11:00 +0200

    fix: modals on mobile
    the js-replacement moves the part to the bottom if the defer-attr
    is set in the script tag.
    
    Now, the buttons get defined first, then the open functions for the models are bound to them.
    
    
    Former-commit-id: f4c0c50e1c359e7d9a680106aa166c15ef17ebd6 [formerly c11ec924825c37de33c4806c2dfe693cbabd7ab7]
    Former-commit-id: b7e672e4875350ca3e94f732fc2dc45a08ee9e55

--- anki/js/modal.js
+++ anki/js/modal.js
@@ -3,34 +3,36 @@ const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
 
 openModalButtons.forEach(button => {
-  button.onclick = () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
-  }
-  })
+button.onclick = () => {
+  openModal(document.querySelector(button.dataset.modalTarget))
+}
+})
 
-overlay.onclick=() => {
-  const modals = document.querySelectorAll('.modal.active')
-  modals.forEach(modal => {
-    closeModal(modal)
-  })}
+overlay.onclick = () => {
+const modals = document.querySelectorAll('.modal.active')
+modals.forEach(modal => {
+  closeModal(modal)
+})
+}
 
 
 closeModalButtons.forEach(button => {
-  button.onclick = () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
-  }})
+button.onclick = () => {
+  const modal = button.closest('.modal')
+  closeModal(modal)
+}
+})
 
 
 function openModal(modal) {
-  if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
+if (modal == null) return
+modal.classList.add('active')
+overlay.classList.add('active')
 }
 
 function closeModal(modal) {
-  if (modal == null) return
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
+if (modal == null) return
+modal.classList.remove('active')
+overlay.classList.remove('active')
 }
+
