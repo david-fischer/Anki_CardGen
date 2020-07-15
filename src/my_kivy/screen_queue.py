67f7ff923eeeb6b1c2c7ef61efc6de4896274cd3
David Fischer <d_fi@gmx.de>
2020-07-16 01:08:19 +0200

    add: doc-strings
    Former-commit-id: c85f781906d4f7f3f4557e1f83da177992514673 [formerly 38f942364db234ec8f34525051b0cfeb21eb6f59]
    Former-commit-id: 10666eebcede886de387be5f70eb8bd2bfeaaec2

--- src/my_kivy/screen_queue.py
+++ src/my_kivy/screen_queue.py
@@ -1,3 +1,7 @@
+"""
+Contains the functions needed on the screen screen_queue.
+"""
+
 import threading
 from queue import Queue
 
@@ -18,11 +22,14 @@ options_dict = {
     "script-text-outline": "Import from Kindle",
     "note-text": "Import from Text File",
 }
+"""
+dict: :code:`{"icon-name":"hint text"}` for the contents of
+:class:`~kivymd.uix.button.MDFloatingActionButtonSpeedDial`.
+"""
 
 
 def worker_single_word():
-    """
-    As long as the queue is not empty, tries to process a single word.
+    """As long as the queue is not empty, tries to process a single word.
     Changing the states accordingly to "loading" and "ready"
     If it fails, it moves the word to the error_words list.
     """
@@ -39,23 +46,27 @@ def worker_single_word():
             MDApp.get_running_app().error_words.append(word)
 
 
-def start_workers(worker, num):
+def start_workers(worker_fn, num):
+    """Starts a number of workers in separate threads
+
+    Args:
+      worker_fn: Function to be started
+      num: Number of threads/workers
+
     """
-    Starts a number of workers in seperate threads
-    :param worker: function to be started
-    :param num:
-    """
-    workers = [threading.Thread(target=worker, name="worker") for _ in range(num)]
-    for w in workers:
-        w.start()
+    workers = [threading.Thread(target=worker_fn, name="worker") for _ in range(num)]
+    for worker in workers:
+        worker.start()
 
 
 def queue_word(word):
     """
     Adds the word to the app.queue_words list and Queue and changes state to "queued".
-    If no worker is present starts a worker in a different thread.
-    :param word:
-    """
+    Then calls :func:`start_downloading`.
+
+    Args:
+      word: Word to add to queue.
+"""
     if not is_duplicate(word):
         MDApp.get_running_app().loading_state_dict[word] = "queued"
         MDApp.get_running_app().queue_words.append(word)
@@ -64,6 +75,12 @@ def queue_word(word):
 
 
 def start_downloading():
+    """
+    Downloads data for queued words:
+
+      * Sets up Queue
+      * If no worker is present, starts one.
+    """
     MDApp.get_running_app().setup_queue()
     if "worker" not in [thread.name for thread in threading.enumerate()]:
         print("starting a worker")
@@ -73,10 +90,25 @@ def start_downloading():
 
 
 def pause_downloading():
+    """
+    Empties queue.
+
+    This leads the download to stop after the current word is processed.
+    """
     MDApp.get_running_app().q = Queue()
 
 
 def is_duplicate(word):
+    """
+    Checks if word is already queued, done or has caused an error.
+
+    Args:
+      word: Word to check.
+
+    Returns:
+      :``True`` if ``word`` is already in app.queue_words, app.error_words or app.done_words.
+      Else ``False``.
+    """
     app = MDApp.get_running_app()
     list_names = ["queue_words", "error_words", "done_words"]
     for name in list_names:
@@ -87,6 +119,11 @@ def is_duplicate(word):
 
 
 def choose_file_to_import(button):
+    """
+    Opens an instance of :class:`~kivymd.uix.filemanager.MDFileManager` so the user can choose a file to import.
+
+    Binds function to clicking on file, so the import is started in separate thread.
+    """
     button.parent.close_stack()
     text = options_dict[button.icon]
     if text == "Import from Kindle":
@@ -103,6 +140,7 @@ def choose_file_to_import(button):
 
 
 def click_on_queue_item(item):
+    """Switch to screen_single_word to generate a card for the clicked word."""
     print(item.text)
     # MDApp.get_running_app().queue_words.remove(item.text)
     set_screen("screen_single_word")
@@ -112,14 +150,25 @@ def click_on_queue_item(item):
 
 
 def click_on_done_item(item):
+    """Placeholder for later implementation."""
     print(item.text)
 
 
 def click_on_error_item(item):
+    """Placeholder for later implementation."""
     print(item.text)
 
 
 def import_from(path, source="kindle"):
+    """
+    Imports, processes and queues words from file.
+
+    If non-dictionary forms are detected, prompts the user with suggestions for replacement.
+
+    Args:
+      path: Path to file.
+      source: Type of source. Options=["kindle","txt"] (Default value = "kindle")
+    """
     MDApp.get_running_app().file_manager.close()
     import_function_dict = {"kindle": word_list_from_kindle, "txt": word_list_from_txt}
     words = import_function_dict[source](path)
@@ -135,6 +184,13 @@ def import_from(path, source="kindle"):
 
 
 def choose_replacements_dialog(replacements):
+    """
+    Displays possible replacements.
+
+    Opens an :class:`~kivymd.uix.dialog.MDDialog` to show possible suggestions for detected non-dictionary forms of
+    words.
+    """
+
     def item_function(obj):
         word = obj.text.split(" -> ")[-1]
         queue_word(word)
