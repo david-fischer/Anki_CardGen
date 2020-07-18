21c38d919606af0c69c7b7c091a8b227fb1f6754
David Fischer <d_fi@gmx.de>
2020-07-19 01:35:14 +0200

    refactor
    Former-commit-id: 7fd908c9be522f15d99d185bf848505ba7550237 [formerly ff2d9357b67ebb4e7caa826da273add82f47ecb0]
    Former-commit-id: d2edb167c170823eed011d4d9b07995319f8e70c

--- src/my_kivy/main_menu.py
+++ src/my_kivy/main_menu.py
@@ -4,6 +4,7 @@ Implements :class:`MainMenu`.
 
 import os
 
+from kivy.factory import Factory
 from kivy.lang import Builder
 from kivy.properties import ListProperty, StringProperty
 from kivy.uix.screenmanager import Screen
@@ -21,25 +22,24 @@ class DrawerItem(CheckBehavior, OneLineIconListItem):
     icon = StringProperty()
     """:class:`~kivy.properties.StringProperty`: name of icon."""
 
-    screen_name = StringProperty("test")
+    name = StringProperty("test")
     """:class:`~kivy.properties.StringProperty`: name of the screen."""
+
+    path = StringProperty("")
+    """:class:`~kivy.properties.StringProperty`: path of the screens kv-file."""
 
     def __init__(self, **kwargs):
         super(DrawerItem, self).__init__(**kwargs)
-        self.theme_cls.bind(theme_style=self.__post_init__)
-        self.theme_cls.bind(primary_palette=self.__post_init__)
-
-    def __post_init__(self, *_):
-        self.state_dicts = {
-            True: {"text_color": self.theme_cls.primary_color},
-            False: {"text_color": self.theme_cls.text_color},
-        }
-        super(DrawerItem, self).__post_init__()
+        self.theme_cls.bind(theme_style=self.on_current_state)
+        self.theme_cls.bind(primary_palette=self.on_current_state)
 
     def on_release(self):
         """Close drawer and sets :attr:`current_state` to ``True``."""
         self.current_state = True
         widget_by_id("nav_drawer").set_state("close")
+
+
+Factory.register("DrawerItem", DrawerItem)
 
 
 class DrawerList(ThemableBehavior, CheckContainer, MDList):
@@ -50,14 +50,14 @@ class DrawerList(ThemableBehavior, CheckContainer, MDList):
     """
 
     check_one = True
-    CheckElementObject = DrawerItem
+    child_class_name = "DrawerItem"
     current = StringProperty("")
     """:class:`~kivy.properties.StringProperty` of currently active :attr:`DrawerItem.screen_name`."""
 
     def conditional_uncheck(self, instance, value):
         """changes color of clicked item and updates :attr:`current`"""
         super(DrawerList, self).conditional_uncheck(instance, value)
-        self.current = instance.screen_name
+        self.current = instance.name
 
 
 class MainMenu(StackLayout):
@@ -68,35 +68,56 @@ class MainMenu(StackLayout):
             {
                 "icon": "form-textbox",
                 "text": "Manual Input",
-                "screen_name": "screen_single_word",
+                "name": "screen_single_word",
+                "path": "my_kivy/screen_single_word.kv",
             },
             {
                 "icon": "format-list-checkbox",
                 "text": "Queue",
-                "screen_name": "screen_queue",
+                "name": "screen_queue",
+                "path": "my_kivy/screen_queue.kv",
             },
-            {"icon": "cogs", "text": "Settings", "screen_name": "screen_settings"},
+            {
+                "icon": "cogs",
+                "text": "Settings",
+                "name": "screen_settings",
+                "path": "my_kivy/screen_settings.kv",
+            },
         ]
     )
     """:class:`~kivy.properties.ListProperty` containing the dictionaries describing all screens."""
 
     def on_parent(self, *_):
         """
-        This function sets up the screens using the words from :attr:`screen_dicts`.
+        This function sets up the screens using ``name`` and ``path`` from :attr:`screen_dicts`.
 
         The screens are added to the screen_man and corresponding entries to the drawer_list.
         Then :attr:`DrawerList.current` is bound to screen_man.current and vice-versa.
         """
         for screen_dict in self.screen_dicts:
-            name = screen_dict["screen_name"]
-            path = f"my_kivy/{name}.kv"
-            screen = Screen(name=name)
-            if not os.path.exists(path):
-                with open(path, "w") as file:
-                    file.write(f'MDLabel:\n\ttext:"{name}"')
-            screen.add_widget(Builder.load_file(path))
+            screen = KvScreen(**{key: screen_dict[key] for key in ["name", "path"]})
             self.ids.screen_man.add_widget(screen)
         self.ids.drawer_list.child_dicts = self.screen_dicts
         self.ids.drawer_list.bind(current=self.ids.screen_man.setter("current"))
         self.ids.screen_man.bind(current=self.ids.drawer_list.setter("current"))
         self.ids.drawer_list.children[-1].on_release()
+
+
+class KvScreen(Screen):
+    path = StringProperty("my_kivy/screen_default.kv")
+
+    def __init__(self, **kwargs):
+        super(KvScreen, self).__init__(**kwargs)
+        if not os.path.exists(self.path):
+            self.create_content_file()
+        self.load_content()
+
+    def load_content(self):
+        self.add_widget(Builder.load_file(self.path))
+
+    def create_content_file(self):
+        with open(self.path, "w") as file:
+            file.write(f'MDLabel:\n\ttext:"{self.name}"')
+
+
+Factory.register("KvScreen", KvScreen)
