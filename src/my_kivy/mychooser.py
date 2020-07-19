fc595357243677c351006126c0b6a29939111fd7
David Fischer <d_fi@gmx.de>
2020-07-19 20:24:58 +0200

    refactoring: moved behaviors to my_kivy.behaviors.py
    Former-commit-id: 7e0ba5beb2db9d4f44307704c98377f51b8471d7 [formerly ceb6b674cec44dd7875b38a3d820ad74300ff800]
    Former-commit-id: e62d5fa623d7e0e4cbf77f36b4068b0880367688

--- src/my_kivy/mychooser.py
+++ src/my_kivy/mychooser.py
@@ -1,19 +1,14 @@
 import os
 from functools import partial
 
-from kivy import clock
 from kivy.animation import Animation
-from kivy.clock import Clock
-from kivy.event import EventDispatcher
 from kivy.factory import Factory
 from kivy.lang import Builder
 from kivy.properties import (
     BooleanProperty,
     DictProperty,
-    ListProperty,
     NumericProperty,
     ObjectProperty,
-    Property,
     StringProperty,
 )
 from kivy.uix.behaviors import ButtonBehavior
@@ -30,107 +25,19 @@ from kivymd.uix.behaviors import CircularRippleBehavior, RectangularRippleBehavi
 from kivymd.uix.card import MDCard
 from kivymd.uix.imagelist import SmartTile
 
+from my_kivy.behaviors import (
+    CheckBehavior,
+    ChildrenFromDictsBehavior,
+    LongPressBehavior,
+    ThemableColorChangeBehavior,
+    TranslationOnCheckBehavior,
+)
+
 try:
     Builder.load_file("my_kivy/mychooser.kv")
 except FileNotFoundError:
     this_directory = os.path.dirname(__file__)
     Builder.load_file(os.path.join(this_directory, "mychooser.kv"))
-
-
-class LongPressBehavior(EventDispatcher):
-    long_press_time = Factory.NumericProperty(1)
-
-    def __init__(self, **kwargs):
-        super(LongPressBehavior, self).__init__(**kwargs)
-        self.register_event_type("on_long_press")
-
-    def on_state(self, instance, value):
-        try:
-            super(LongPressBehavior, self).on_state(instance, value)
-        except AttributeError:
-            pass
-        if value == "down":
-            lpt = self.long_press_time
-            self._clockev = Clock.schedule_once(self._do_long_press, lpt)
-        else:
-            self._clockev.cancel()
-
-    def _do_long_press(self, dt):
-        self.dispatch("on_long_press")
-
-    def on_long_press(self, *largs):
-        pass
-
-
-class MultiStateBehaviour:
-    current_state = Property(None)
-    """:class:`~kivy.properties.Property`"""
-
-    state_dicts = DictProperty(None)
-    """:class:`~kivy.properties.DictProperty`"""
-
-    animated_properties = ListProperty()
-    """:class:`~kivy.properties.ListProperty`"""
-
-    def __init__(self, **kwargs):
-        super(MultiStateBehaviour, self).__init__(**kwargs)
-        clock.Clock.schedule_once(self.__post_init__)
-
-    def on_current_state(self, *_):
-        animation_dict = {
-            key: val
-            for key, val in self.state_dicts[self.current_state].items()
-            if key in self.animated_properties
-        }
-        for key, val in self.state_dicts[self.current_state].items():
-            if key not in self.animated_properties:
-                setattr(self, key, val)
-        if animation_dict:
-            anim = Animation(**animation_dict, duration=0.5, t="out_circ")
-            anim.start(self)
-
-    def __post_init__(self, *_):
-        self.on_current_state()
-
-
-class CheckBehavior(MultiStateBehaviour):
-    def __init__(self, **kwargs):
-        self.current_state = False
-        self.state_dicts = (
-            {True: {}, False: {}} if self.state_dicts is None else self.state_dicts
-        )
-        super(CheckBehavior, self).__init__(**kwargs)
-
-
-class ChildrenFromDictsBehavior:
-    child_dicts = ListProperty([])
-    child_class_name = StringProperty([])
-    root_for_children = ObjectProperty()
-    child_bindings = DictProperty()
-    parent_bindings = DictProperty()
-
-    def __init__(self, **kwargs):
-        super(ChildrenFromDictsBehavior, self).__init__(**kwargs)
-        if self.root_for_children is None:
-            self.root_for_children = self
-        self.on_child_dicts()
-
-    def on_child_dicts(self, *_):
-        self.root_for_children.clear_widgets()
-        for child_dict in self.child_dicts:
-            child_cls = Factory.get(self.child_class_name)
-            new_child = child_cls(**child_dict)
-            if self.child_bindings:
-                new_child.bind(**self.child_bindings)
-            self.before_add_child(new_child)
-            self.root_for_children.add_widget(new_child)
-            self.after_add_child(new_child)
-
-    def before_add_child(self, child):
-        """Function to be executed before child is added to :attr:`parent_widget`."""
-
-    def after_add_child(self, child):
-        """Function to be executed before child is added to :attr:`parent_widget`."""
 
 
 class CheckContainer(ChildrenFromDictsBehavior):
@@ -157,45 +64,6 @@ class CheckContainer(ChildrenFromDictsBehavior):
         if property_name is None:
             return checked_elements
         return [getattr(element, property_name) for element in checked_elements]
-
-
-class TranslationOnCheckBehavior:
-    text_orig = StringProperty("orig")
-    """:class:`~kivy.properties.StringProperty`"""
-
-    text_trans = StringProperty("trans")
-    """:class:`~kivy.properties.StringProperty`"""
-
-    def __post_init__(self, *_):
-        self.state_dicts[True]["text"] = self.text_orig
-        self.state_dicts[False]["text"] = self.text_trans
-        super(TranslationOnCheckBehavior, self).__post_init__(*_)
-
-
-class ThemableColorChangeBehavior:
-    text_color = ListProperty([0, 0, 0, 1])
-    """:class:`~kivy.properties.ListProperty`"""
-
-    bg_color = ListProperty([1, 1, 1, 1])
-    """:class:`~kivy.properties.ListProperty`"""
-
-    animated_properties = ["bg_color", "text_color"]
-
-    def __init__(self, **kwargs):
-        super(ThemableColorChangeBehavior, self).__init__(**kwargs)
-        self.theme_cls.bind(theme_style=self.__post_init__)
-        self.theme_cls.bind(primary_palette=self.__post_init__)
-
-    def __post_init__(self, *_):
-        self.state_dicts[True]["bg_color"] = self.theme_cls.primary_color
-        self.state_dicts[False]["bg_color"] = (
-            self.theme_cls.bg_darkest
-            if self.theme_cls.theme_style == "Light"
-            else self.theme_cls.bg_light
-        )
-        self.state_dicts[True]["text_color"] = [1, 1, 1, 1]
-        self.state_dicts[False]["text_color"] = self.theme_cls.secondary_text_color
-        super(ThemableColorChangeBehavior, self).__post_init__()
 
 
 class MyCheckCard(ThemableColorChangeBehavior, CheckBehavior, MDCard):
