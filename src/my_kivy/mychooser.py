10fbbab3bf3fc60729745cddc151bd3b7d2656e3
David Fischer <d_fi@gmx.de>
2020-07-22 15:39:00 +0200

    refactor + docstrings
    Former-commit-id: 7d25704af5e7af477e44fc9ce7c5bb875e44d273

--- src/my_kivy/mychooser.py
+++ src/my_kivy/mychooser.py
@@ -1,3 +1,4 @@
+"""Implements various elements to get user selection."""
 import os
 from functools import partial
 
@@ -6,9 +7,9 @@ from kivy.factory import Factory
 from kivy.lang import Builder
 from kivy.properties import (
     BooleanProperty,
-    DictProperty,
     NumericProperty,
     ObjectProperty,
+    OptionProperty,
     StringProperty,
 )
 from kivy.uix.behaviors import ButtonBehavior
@@ -16,12 +17,13 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import AsyncImage
 from kivy.uix.modalview import ModalView
-from kivy.uix.recycleview import RecycleView
-from kivy.uix.scrollview import ScrollView
 from kivy.uix.stacklayout import StackLayout
 from kivymd.app import MDApp
 from kivymd.theming import ThemableBehavior
-from kivymd.uix.behaviors import CircularRippleBehavior, RectangularRippleBehavior
+from kivymd.uix.behaviors import (
+    CircularRippleBehavior,
+    RectangularRippleBehavior,
+)
 from kivymd.uix.card import MDCard
 from kivymd.uix.imagelist import SmartTile
 
@@ -41,41 +43,57 @@ except FileNotFoundError:
 
 
 class CheckContainer(ChildrenFromDictsBehavior):
-    """Test"""
+    """Container for widgets with :class:`~my_kivy.behaviors.CheckBehavior`."""
 
     check_one = BooleanProperty(False)
-    """:class:`~kivy.properties.BooleanProperty`"""
+    """:class:`~kivy.properties.BooleanProperty` defaults to ``False``. If ``True`` only one child can be selected."""
 
     def __init__(self, **kwargs):
         super(CheckContainer, self).__init__(**kwargs)
         self.child_bindings["current_state"] = self.conditional_uncheck
 
     def conditional_uncheck(self, instance, value):
+        """Uncheck other widgets if :attr:`check_one` is ``True``."""
         if self.check_one:
             for check_element in [
                 others for others in self.children if others != instance and value
             ]:
                 check_element.current_state = False
 
-    def get_checked(self, property_name=None):
+    def get_checked(self, attribute_name=None):
+        """
+        Return current selection.
+
+        Args:
+            attribute_name: Name of attribute to return. Defaults to ``None``.
+
+        Returns:
+            :* If ``attribute_name`` is None:  List of selected children
+            :* Else: List of attribute values
+        """
         checked_elements = [
             element for element in self.children[::-1] if element.current_state
         ]
-        if property_name is None:
+        if attribute_name is None:
             return checked_elements
-        return [getattr(element, property_name) for element in checked_elements]
+        return [getattr(element, attribute_name) for element in checked_elements]
 
 
 class MyCheckCard(ThemableColorChangeBehavior, CheckBehavior, MDCard):
+    """Selectable :~kivymd.uix.card.MDCard`. Select by click. Changes color on selection."""
+
     text = StringProperty("test " * 15)
-    """:class:`~kivy.properties.StringProperty`"""
+    """:class:`~kivy.properties.StringProperty`."""
 
     def on_press(self):
-        self.current_state = not self.current_state
+        """Change boolean value of :attr:`self.current_state`."""
+        self.current_state = (  # pylint: disable=attribute-defined-outside-init
+            not self.current_state
+        )
 
 
 class MyTransCard(TranslationOnCheckBehavior, MyCheckCard):
-    pass
+    """Selectable :class:`~kivymd.uix.card.MDCard`. Select by click. Changes color and displayed text on click."""
 
 
 class MyCheckChip(
@@ -86,27 +104,40 @@ class MyCheckChip(
     ThemableBehavior,
     BoxLayout,
 ):
-    icon = StringProperty("")
-    """:class:`~kivy.properties.StringProperty`"""
+    """Selectable Chip. Select by click. Change color on selection."""
 
-    text = StringProperty("Chip")
-    """:class:`~kivy.properties.StringProperty`"""
+    icon = StringProperty("")
+    """:class:`~kivy.properties.StringProperty` defaults to ""."""
+
+    text = StringProperty("")
+    """:class:`~kivy.properties.StringProperty` defaults to ""."""
 
     def on_press(self):
-        self.current_state = not self.current_state
+        """Change boolean value of :attr:`current_state`."""
+        self.current_state = (  # pylint: disable=attribute-defined-outside-init
+            not self.current_state
+        )
 
 
 class MyTransChip(TranslationOnCheckBehavior, MyCheckChip):
-    pass
+    """Selectable Chip. Select by click. Change color and text on selection."""
 
 
 class MyCheckChipContainer(CheckContainer, ThemableBehavior, StackLayout):
+    """Container for :class:`MyCheckChip`s. Use :attr:`child_dict` to populate."""
+
     child_class_name = "MyCheckChip"
 
 
 class MyCheckImageTile(CheckBehavior, SmartTile):
+    """
+    Selectable :class:`~kivymd.uix.imagelist.SmartTile".
+
+    Select by click. Changes :attr:`opacity` and :attr:`boarder_width` on selection.
+    """
+
     border_width = NumericProperty(0.01)
-    """:class:`~kivy.properties.NumericProperty`"""
+    """:class:`~kivy.properties.NumericProperty` describing boarder-width of image tile."""
 
     def __init__(self, **kwargs):
         self.state_dicts = {
@@ -116,46 +147,49 @@ class MyCheckImageTile(CheckBehavior, SmartTile):
         super(MyCheckImageTile, self).__init__(**kwargs)
 
     def on_press(self):
-        self.current_state = not self.current_state
+        """Change boolean value of current state on press."""
+        self.current_state = (  # pylint: disable=attribute-defined-outside-init
+            not self.current_state
+        )
 
 
 class TransCard(LongPressBehavior, MDCard, RectangularRippleBehavior):
+    """Displays :attr:`text_orig` and :attr:`text_trans`, separated by a line."""
+
     text_orig = StringProperty("")
+    """:class:`~kivy.properties.StringProperty` first text."""
+
     text_trans = StringProperty("")
+    """:class:`~kivy.properties.StringProperty` second text."""
+
+    orientation = OptionProperty("vertical", options=["vertical", "horizontal"])
+    """:class:`~kivy.properties.OptionProperty` possible values ["vertical", "horizontal"] defaults to "vertical"."""
 
 
-class LongPressImage(ButtonBehavior, LongPressBehavior, AsyncImage):
-    pass
+class LongPressImage(LongPressBehavior, AsyncImage):
+    """:class:`~kivy.uix.image.AsyncImage` with additional "on_press" and "on_long_press" event."""
 
 
 Factory.register("LongPressImage", LongPressImage)
 Factory.register("TransCard", TransCard)
 
 
-class ScrollBox(ChildrenFromDictsBehavior, ScrollView):
-    pass
-
-
-class ScrollGrid(ChildrenFromDictsBehavior, ScrollView):
-    pass
-
-
-Factory.register("ScrollBox", ScrollBox)
-Factory.register("ScrollGrid", ScrollGrid)
-
-
-class RecycleViewBox(RecycleView):
-    pass
-
-
-Factory.register("RecycleViewBox", RecycleViewBox)
-
-
 class MyCarousel(FloatLayout, ChildrenFromDictsBehavior):
+    """
+    Carousel that constructs contents from child_dicts.
+
+    On click, opens a modal with list of content.
+    """
+
     carousel = ObjectProperty()
-    height_dict = DictProperty()
+    """:class:`~kivy.properties.ObjectProperty`"""
+
     recycle_view_name = StringProperty()
+    """:class:`~kivy.properties.StringProperty`"""
+
     recycle_view_data_class = StringProperty()
+    """:class:`~kivy.properties.StringProperty`"""
+
     modal = ModalView()
 
     def __init__(self, **kwargs):
@@ -167,19 +201,24 @@ class MyCarousel(FloatLayout, ChildrenFromDictsBehavior):
         self.on_child_dicts()
 
     def before_add_child(self, child):
+        """Bind :meth:`set_child_width` to change of :attr:`width`."""
         self.bind(width=lambda *_: self.set_child_width(child))
 
     def after_add_child(self, child):
+        """Call :meth:`set_child_width` after adding child."""
         self.set_child_width(child)
 
     def set_child_width(self, child, *_):
+        """Set width of child to :attr:`width` - width of left and right-icon."""
         width = self.width - self.ids.left_icon.width - self.ids.right_icon.width
         setattr(child, "width", width)
 
     def update_height(self, *_):
-        pass
+        """Implement in sub class. Placeholder."""
 
     def get_modal_content(self, size_hint=(1, None)):
+        """Return root widget to display on the modal."""
+
         def set_carousel_index(i, *_):
             self.carousel.index = i
             self.modal.dismiss()
@@ -194,17 +233,15 @@ class MyCarousel(FloatLayout, ChildrenFromDictsBehavior):
         recycle_view.child_dicts = data_dicts
         return recycle_view
 
-    def on_child_dicts(self, *_):
-        self.height_dict = {}
-        super(MyCarousel, self).on_child_dicts(*_)
-
     def get_checked(self, property_name=None):
+        """If ``attribute_name`` is ``None``, return currently selected widget, else return a property thereof."""
         checked_elements = [self.carousel.current_slide]
         if property_name is None:
             return checked_elements
         return [getattr(element, property_name) for element in checked_elements]
 
     def open_menu(self, *_):
+        """Open :class:`kivy.uix.modalview.ModalView` with content given by :meth:`get_modal_content`."""
         self.modal = ModalView()
         modal_content = self.get_modal_content()
         self.modal.add_widget(modal_content)
@@ -212,12 +249,22 @@ class MyCarousel(FloatLayout, ChildrenFromDictsBehavior):
 
 
 class ImageCarousel(MyCarousel):
+    """Carousel of images."""
+
     def get_modal_content(self, size_hint=(1, 1)):
+        """Call :meth:`MyCarousel.get_modal_content` with ``size_hint=(1,1)``."""
         return super(ImageCarousel, self).get_modal_content(size_hint=size_hint)
 
 
 class CardCarousel(MyCarousel):
+    """
+    Carousel of :class:`TransCard`.
+
+    To use it with different objects, change :attr:`child_class_name` and :attr:`recycle_view_data_class`.
+    """
+
     def update_height(self, *_):
+        """Update height via animation, so that Widget has height of currently displayed card."""
         if self.carousel.current_slide:
             new_height = self.carousel.current_slide.height + 24
             if self.height != new_height:
@@ -225,14 +272,15 @@ class CardCarousel(MyCarousel):
                 anim.start(self)
 
     def on_child_dicts(self, *_):
+        """Fix size-issue on first init."""
         super(CardCarousel, self).on_child_dicts(*_)
         if self.carousel:
             self.carousel.index = 1
             self.carousel.index = 0
 
 
+# pylint: disable = W,C,R,I
 if __name__ == "__main__":
-
     CARD_CAROUSEL_STRING = (
         "BoxLayout:\n"
         "    CardCarousel:\n"
@@ -245,10 +293,10 @@ if __name__ == "__main__":
         '        child_dicts: [{"source":"../assets/AnkiCardGen.png"} for _ in range(5)]'
     )
 
-    class TestApp(MDApp):
+    class _TestApp(MDApp):
         def build(self):
             self.theme_cls.primary_palette = "Red"  # "Purple", "Red"
             self.theme_cls.theme_style = "Light"  # "Purple", "Red"
             return Builder.load_string(CARD_CAROUSEL_STRING)
 
-    TestApp().run()
+    _TestApp().run()
