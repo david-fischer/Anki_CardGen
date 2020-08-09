5fd0c344d6efab87ea686b43061a860fd315bde8
David Fischer <d_fi@gmx.de>
2020-08-10 01:28:07 +0200

    add: export function to history screen
    Former-commit-id: 91e28cb6967397c959b2f8b7f8c49ca72a27f00c [formerly 072b7cea904fa6f36e0cb7d9f6488a8eca7be1a6]
    Former-commit-id: 6fdaacdf54fd2104200f067ecbb8ee7c1fd78d34

---
+++ src/screens/history.py
@@ -0,0 +1,58 @@
+"""Implements :class:`HistoryMain`, the root widget of the history screen."""
+from bidict import bidict
+from kivy.clock import Clock
+from kivy.properties import ObjectProperty
+from kivy.uix.floatlayout import FloatLayout
+from kivymd.app import MDApp
+from kivymd.toast import toast
+from pony.orm import db_session
+
+from db import export_cards, get_template
+
+
+class HistoryMain(FloatLayout):
+    """Root widget of the history screen."""
+
+    action_button_data = bidict(
+        {
+            "new-box": "export new cards",
+            "content-save-all": "export all cards",
+            "check-box-multiple-outline": "select specific cards to export",
+        }
+    )
+    speed_dial = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        super(HistoryMain, self).__init__(**kwargs)
+        Clock.schedule_once(self.__post_init__)
+
+    def __post_init__(self, *_):
+        self.speed_dial.icon = "content-save"
+
+    def export_cards(self, button):
+        """Depending on which button was pressed, exports one of the following.
+
+        * all cards
+        * all new cards (i.e. cards that have been generated but not exported)
+        * custom selection of cards (NOT YET IMPLEMENTED)
+        TODO: IMPLEMENT
+        """
+        self.speed_dial.close_stack()
+        option = self.action_button_data[button.icon]
+        if option == "export new cards":
+            states = ["done"]
+        elif option == "export all cards":
+            states = ["done", "exported"]
+        else:
+            toast("NOT IMPLEMENTED YET.", 10)
+            return False
+        with db_session:
+            current_template = get_template(MDApp.get_running_app().current_template)
+            cards = current_template.get_cards_by_selector(lambda c: c.state in states)
+            if not cards:
+                toast("Empty Selection. Could not export cards.")
+                return False
+            export_dir = MDApp.get_running_app().apkg_export_dir
+            export_cards(cards, export_dir)
+        toast(f"Exported cards to {export_dir}.", 5)
+        return True
