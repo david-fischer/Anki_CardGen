e3f9c1a686076100a0d5bde7ca27a06d36e2e609
David Fischer <d_fi@gmx.de>
2020-08-10 01:23:39 +0200

    update: single_word now uses template- instead of word-class
    Former-commit-id: f0334042481e70c28d89eaf0d0d488eb2fd45aee

--- src/screens/single_word.py
+++ src/screens/single_word.py
@@ -1,161 +1,28 @@
-"""Contains the necessary functions and classes for the screen single_word."""
-import os
+"""Implements root widget of the single_word screen."""
+import pydoc
 
-import certifi
-from kivy.lang import Builder
-from kivy.network.urlrequest import UrlRequest
+from kivy.clock import Clock
+from kivy.properties import ObjectProperty
 from kivy.uix.floatlayout import FloatLayout
 from kivymd.app import MDApp
 
-from parsers import linguee_did_you_mean, NoMatchError
-from utils import (
-    compress_img,
-    set_screen,
-    tag_word_in_sentence,
-)
-
-os.environ["SSL_CERT_FILE"] = certifi.where()
+from db import get_template
 
 
-class WordProperties(FloatLayout):
-    """
-    UI for picking content of Anki card.
+class SingleWordMain(FloatLayout):
+    """Root widget of the single_word screen."""
 
-    Consists of the UI-elements that display the data obtained by :class:`words.Word` and lets the user choose which
-    data to include on the card.
-    """
+    scroll_view = ObjectProperty()
 
-    def refresh_data(self):
-        """Refresh UI with the data from :class:`words.Word`."""
-        print(self.parent.parent.ids)
-        word = MDApp.get_running_app().word
-        self.ids.translation.data = [{"text": string} for string in word.translation]
-        self.ids.image.data = [{"source": string} for string in word.image]
-        for attribute in ["antonym", "synonym", "example", "explanation"]:
-            self.ids[attribute].data = [
-                {"text_orig": x[0], "text_trans": x[1]}
-                for x in getattr(word, attribute)
-            ]
+    def __init__(self, **kwargs):
+        super(SingleWordMain, self).__init__(**kwargs)
+        Clock.schedule_once(self.__post_init__)
 
-    def accept_suggestion(self, suggestion):
-        """Use Suggestion for new search."""
-        self.ids.search_field.text = suggestion
-        self.load_or_search(suggestion)
-
-    def load_or_search(self, search_term):
-        """Search for word, suggest alternative if not found and show images."""
-        try:
-            MDApp.get_running_app().word.search(search_term)
-            self.refresh_data()
-            self.parent.scroll_y = 1
-        except NoMatchError as error:
-            suggestions = linguee_did_you_mean(search_term)
-            MDApp.get_running_app().show_dialog(
-                message=f"{search_term} not found on {error.site}."
-                + (" Did you mean... ?" if suggestions else ""),
-                options=suggestions,
-                item_callback=lambda obj: self.accept_suggestion(obj.text),
-                close_on_item=True,
-            )
-
-    def get_user_selection_dict(self):
-        """
-        Return current user selection as dict (in the form needed by :attr:`main.AnkiCardGenApp.add_anki_card`).
-
-        Example and explanation strings are tagged using :func:`utils.tag_word_in_sentence`.
-        """
-        selections = [
-            {"key": "translation", "id": "translation", "attr": "text"},
-            {"key": "synonym", "id": "synonym", "attr": "text_orig"},
-            {"key": "antonym", "id": "antonym", "attr": "text_orig"},
-            {"key": "explanation", "id": "explanation", "attr": "text_orig"},
-            {"key": "example", "id": "example", "attr": "text_orig"},
-            {"key": "example_translation", "id": "example", "attr": "text_trans"},
-        ]
-        result_dict = {
-            d["key"]: ",".join(self.ids[d["id"]].get_checked(attribute_name=d["attr"]))
-            for d in selections
-        }
-        for key in ["example", "explanation"]:
-            result_dict[key] = tag_word_in_sentence(
-                result_dict[key], MDApp.get_running_app().word.search_term
-            )
-        print(result_dict)
-        return result_dict
-
-    def download_selected_image(self):
-        """Download the currently selected image to :meth:`words.Word.base_path` + ".jpg"."""
-        # TODO: show spinner and cancel after certain time
-        # TODO: Correct behavior on Error
-        out_path = MDApp.get_running_app().word.base_path() + ".jpg"
-        img_url = self.ids.image.get_checked(attribute_name="source")[0]
-        r_i = UrlRequest(
-            img_url,
-            file_path=out_path,
-            on_success=lambda *args: compress_img(out_path),
-        )
-        r_i.wait()
-
-    def complete_generation(self):
-        """
-        Generate Anki card from current user selection and changes screen.
-
-        If the word was in the queue, returns to queue.
-        Else, resets the single_word and changes to the edit_tab.
-        """
-        self.download_selected_image()
-        word = MDApp.get_running_app().word
-        result_dict = {
-            **word.get_fix_card_dict(),
-            **self.get_user_selection_dict(),
-        }
-        MDApp.get_running_app().add_anki_card(result_dict)
-        self.ids.search_field.text = ""
-        word.__init__()
-        self.refresh_data()
-        # TODO: Why does the focussing not work anymore? -> Clock.schedule_once
-        self.ids.search_field.focus = True
-        self.parent.scroll_y = 1
-        search_term = result_dict["word"]
-        if search_term in MDApp.get_running_app().word_state_dict:
-            set_screen("queue")
-            MDApp.get_running_app().word_state_dict[search_term] = "done"
-
-
-# class ImageSearchResultGrid(MyCheckImageGrid):
-#     """Extends the :class:`selection_widgets.MyCheckImageGrid` by the :meth:`get_images` method."""
-#
-#     def on_data(self, *_):
-#         if len(self.children) == len(self.data):
-#             for image, child_dict in zip(self.children, self.data):
-#                 image.source = child_dict["source"]
-#                 # image._img_widget.container.image.bind(on_error=f)
-#         else:
-#             super(ImageSearchResultGrid, self).on_data(*_)
-#
-#     def get_images(self, keywords=None):
-#         """
-#         Sets images displayed in :class:`ImageSearchResultGrid`.
-#
-#         Args:
-#             keywords:
-#                 If None, uses :attr:`words.Word.img_urls`, else uses result of :meth:`words.Word.request_img_urls`
-#                 for given keywords. (Default = None)
-#         """
-#         word = MDApp.get_running_app().word
-#         paths = (
-#             word.image_urls
-#             if keywords is None
-#             else word.request_img_urls(keywords=keywords)
-#         )
-#         self.data = [{"source": url} for url in paths]
-
-
-# pylint: disable = W,C,R,I
-if __name__ == "__main__":
-
-    class _TestApp(MDApp):
-        def build(self):
-            return Builder.load_file("single_word.kv")
-
-    _TestApp().run()
+    def __post_init__(self, *_):
+        app = MDApp.get_running_app()
+        current_template = app.current_template
+        template_cls_name = get_template(current_template).cls_name
+        template_cls = pydoc.locate(template_cls_name)
+        template = template_cls()
+        template.add_field_widgets()
+        self.scroll_view.add_widget(template)
