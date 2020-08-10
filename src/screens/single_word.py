50d2725b4d1f8b4b9158f8322758eb229ff0bf89
David Fischer <d_fi@gmx.de>
2020-08-10 19:39:39 +0200

    refactor: move adding of template to single_word screen
    Former-commit-id: 904a718c933fd72959f9065d21c2cf4745e90cba

--- src/screens/single_word.py
+++ src/screens/single_word.py
@@ -8,3 +8,9 @@ class SingleWordRoot(FloatLayout):
     """Root widget of the single_word screen."""
 
     scroll_view = ObjectProperty()
+    template = ObjectProperty()
+
+    def on_template(self, *_):
+        """Update :attr:`scroll_view` if self.template changes."""
+        self.scroll_view.clear_widgets()
+        self.scroll_view.add_widget(self.template)
