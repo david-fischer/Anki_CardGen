d4ce6ea372eeac12e2b78a035738b7591b90efe3
David Fischer <d_fi@gmx.de>
2020-08-10 14:51:05 +0200

    refactor: unify screen structure
    Former-commit-id: 7092acb606fa919c2d7f2c8fedfdad49cfca1f10
    Former-commit-id: 4170971508b0f01f385a5795cea877778f9c1547

---
+++ src/screens/settings.py
@@ -0,0 +1,6 @@
+"""Implements :class:`SettingsRoot`, the root widget for the settings screen."""
+from kivy.uix.boxlayout import BoxLayout
+
+
+class SettingsRoot(BoxLayout):
+    """Root widget of the settings screen."""
--- src/screens/single_word.py
+++ src/screens/single_word.py
@@ -4,7 +4,7 @@ from kivy.properties import ObjectProperty
 from kivy.uix.floatlayout import FloatLayout
 
 
-class SingleWordMain(FloatLayout):
+class SingleWordRoot(FloatLayout):
     """Root widget of the single_word screen."""
 
     scroll_view = ObjectProperty()
