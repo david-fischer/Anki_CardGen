16b1bef7265e3b5924ef7fd35842d23b0ba99f52
David Fischer <d_fi@gmx.de>
2020-08-10 02:03:30 +0200

    refactor: move setup of template class to app
    Former-commit-id: dacaffa727d0be0744da72b087d88dbe9ad6cf10

--- src/screens/single_word.py
+++ src/screens/single_word.py
@@ -1,28 +1,10 @@
 """Implements root widget of the single_word screen."""
-import pydoc
 
-from kivy.clock import Clock
 from kivy.properties import ObjectProperty
 from kivy.uix.floatlayout import FloatLayout
-from kivymd.app import MDApp
-
-from db import get_template
 
 
 class SingleWordMain(FloatLayout):
     """Root widget of the single_word screen."""
 
     scroll_view = ObjectProperty()
-
-    def __init__(self, **kwargs):
-        super(SingleWordMain, self).__init__(**kwargs)
-        Clock.schedule_once(self.__post_init__)
-
-    def __post_init__(self, *_):
-        app = MDApp.get_running_app()
-        current_template = app.current_template
-        template_cls_name = get_template(current_template).cls_name
-        template_cls = pydoc.locate(template_cls_name)
-        template = template_cls()
-        template.add_field_widgets()
-        self.scroll_view.add_widget(template)
