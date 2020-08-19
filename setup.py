004137d6f048f009fa53d06dffb4cb03c2c058aa
David Fischer <d_fi@gmx.de>
2020-08-19 13:10:56 +0200

    Bump version: 1.0.2 → 1.0.3
    Former-commit-id: e56e6405614f2a8b690c4cdd0c77812866822366

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ACG",
-    version="1.0.2",
+    version="1.0.3",
     packages=[""],
     package_dir={"": "src"},
     url="https://github.com/david-fischer/Anki_CardGen",
