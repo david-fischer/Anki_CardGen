298e0502046525753cea9211a9ce8cc3e2c3de93
David Fischer <d_fi@gmx.de>
2020-08-19 12:40:26 +0200

    add: setup.py
    Former-commit-id: 93e0d0701d46af34191c5a295f3d22e2d66beac6

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="ACG",
+    version="1.0.0",
+    packages=[""],
+    package_dir={"": "src"},
+    url="https://github.com/david-fischer/Anki_CardGen",
+    license="MIT",
+    author="david-fischer",
+    author_email="d.fischer.git@posteo.de",
+    description="Kivy application to generate flash cards for Anki",
+)
