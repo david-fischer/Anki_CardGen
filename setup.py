13e5809342779c8bcf2233ae6461b6df089f75a1
David Fischer <d_fi@gmx.de>
2020-08-19 13:33:35 +0200

    test workflow output
    Former-commit-id: dec35becdaefc35ef296b2411684bb1cecf49cb7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ACG",
-    version="1.0.4",
+    version="1.0.5",
     packages=[""],
     package_dir={"": "src"},
     url="https://github.com/david-fischer/Anki_CardGen",
