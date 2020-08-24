81a88429de3c203151dac17ae412b08654c80a29
David Fischer <d_fi@gmx.de>
2020-08-24 22:01:02 +0200

    update: requirements.txt and setup.py
    Former-commit-id: 50ae36ec092a74122711843ec41a4a79102a2640

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from setuptools import setup
 
+
+def get_requirements():
+    with open("requirements.txt", "r") as file:
+        reqs = [req.strip() for req in file.readlines()]
+    return [req for req in reqs if req]
+
+
+print(f"requirements: {get_requirements()}")
+
 setup(
     name="ACG",
     version="1.0.5",
-    packages=[""],
-    package_dir={"": "src"},
+    packages=["acg"],
+    package_dir={"acg": "src"},
+    include_package_data=True,
+    install_requires=get_requirements(),
     url="https://github.com/david-fischer/Anki_CardGen",
+    keywords="kivy, anki, flash cards, languages, portuguese, learning",
     license="MIT",
     author="david-fischer",
     author_email="d.fischer.git@posteo.de",
