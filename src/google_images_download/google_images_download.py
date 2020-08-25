53c6cca5a0a09aa28e98aa39cd5a6ec59b424f7b
David Fischer <d_fi@gmx.de>
2020-08-25 14:01:51 +0200

    fix: json.decoder.JSONDecodeError: Extra data
    google seems to have updated their response format
    
    
    Former-commit-id: b8860b3642e7e2e762f85d602eab58d25e0afe73

--- src/google_images_download/google_images_download.py
+++ src/google_images_download/google_images_download.py
@@ -1429,7 +1429,8 @@ class googleimagesdownload:
         start_object = s.find("[", start_line + 1)
         end_object = s.find("</script>", start_object + 1) - 4
         object_raw = str(s[start_object:end_object])
-        object_decode = bytes(object_raw, "utf-8")[:-1].decode("unicode_escape")
+        object_decode = bytes(object_raw, "utf-8").decode("unicode_escape")
+        # object_decode = object_decode[: object_decode.rfind("\n")]
         image_objects = json.loads(object_decode)[31][0][12][2]
         image_objects = [x for x in image_objects if x[0] == 1]
         return image_objects
