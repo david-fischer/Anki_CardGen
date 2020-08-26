8a62d40c4b662c1e46ee1d70af2e4021ef2094ed
David Fischer <d_fi@gmx.de>
2020-08-26 16:54:25 +0200

    add: constants for different paths in the app
    this was necessary for the app to work as a single-file executable, installable package and apk at the same time.
    
    
    Former-commit-id: ac248f9bb16617479dd55be8a5c9552a88c7498b

---
+++ src/paths.py
@@ -0,0 +1,9 @@
+"""This module defines constants for the different paths used in the application."""
+
+import os
+
+MAIN_DIR = os.path.dirname(__file__)
+ASSETS_DIR = os.path.join(MAIN_DIR, "assets")
+ANKI_DIR = os.path.join(MAIN_DIR, "anki")
+SCREEN_DIR = os.path.join(MAIN_DIR, "screens")
+CUSTOM_WIDGET_DIR = os.path.join(MAIN_DIR, "custom_widgets")
