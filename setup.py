8600abc81122b96d261bd34a0c2e3261f1da8a17
David Fischer <d_fi@gmx.de>
2020-08-26 16:55:42 +0200

    fix: correct location of submodules and data
    Former-commit-id: a71f28fc6afba3282fbae3739b2283d323e64ea9

--- setup.py
+++ setup.py
@@ -4,18 +4,21 @@ from setuptools import setup
 def get_requirements():
     with open("requirements.txt", "r") as file:
         reqs = [req.strip() for req in file.readlines()]
+    for i, line in enumerate(reqs):
+        if line.startswith("git+"):
+            dep_name = line.rsplit("/")[-1].rsplit(".")[0].lower()
+            reqs[i] = f"{dep_name} @ {line}"
     return [req for req in reqs if req]
 
 
-print(f"requirements: {get_requirements()}")
-
 setup(
-    name="ACG",
+    name="acg",
     version="1.0.5",
-    packages=["acg"],
     package_dir={"acg": "src"},
+    packages=["acg", "acg.google_images_download", "acg.custom_widgets", "acg.screens"],
     include_package_data=True,
     install_requires=get_requirements(),
+    entry_points={"gui_scripts": ["acg = acg.main:main",]},
     url="https://github.com/david-fischer/Anki_CardGen",
     keywords="kivy, anki, flash cards, languages, portuguese, learning",
     license="MIT",
