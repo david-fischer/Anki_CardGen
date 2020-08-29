c634301d7016ce39d43082dda308a01d7bd215e2
David Fischer <d_fi@gmx.de>
2020-08-29 20:16:42 +0200

    add: entry-point for pyinstaller
    Former-commit-id: 0f31a8dc47c06db61d437dec689f671ff028156d
    Former-commit-id: af8f40025e8b74754eeabfedad9ed4d57b0c1fbc

---
+++ acg/cli.py
@@ -0,0 +1,13 @@
+"""
+This is the entry-point for pyinstaller.
+
+It makes sure that the script is called from the right path,
+such that the settings file ``ankicardgen.ini`` is placed in the folder of the main script as opposed to the folder
+from which the script is called.
+"""
+from main import main
+from paths import MAIN_DIR
+from utils import CD
+
+with CD(MAIN_DIR):
+    main()
