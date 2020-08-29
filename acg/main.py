ec76377eedb3de1928294680a66752455e91dfa5
David Fischer <d_fi@gmx.de>
2020-08-29 16:47:49 +0200

    update: implemented theme as ConfigParserProperties
    Former-commit-id: 22e0d075fe837ac855b4ae243b11479a810aaed6

--- acg/main.py
+++ acg/main.py
@@ -9,25 +9,22 @@ from kivy import platform
 from kivy.clock import mainthread
 from kivy.lang import Builder
 from kivy.properties import (
+    AliasProperty,
     BooleanProperty,
     ConfigParserProperty,
-    DictProperty,
     ObjectProperty,
     StringProperty,
 )
 from kivy.uix.modalview import ModalView
 from kivymd.app import MDApp
-from kivymd.theming import ThemeManager
 from kivymd.uix.spinner import MDSpinner
 from pony.orm import db_session
 
 from custom_widgets.main_menu import MainMenu
 from db import add_missing_templates, get_template
 from paths import ANKI_DIR, CUSTOM_WIDGET_DIR
-from utils import smart_loader, smart_saver
 
 os.environ["SSL_CERT_FILE"] = certifi.where()
-
 
 Builder.load_file(os.path.join(CUSTOM_WIDGET_DIR, "main_menu.kv"))
 Builder.load_file(os.path.join(CUSTOM_WIDGET_DIR, "fixes.kv"))
@@ -37,14 +34,17 @@ class AnkiCardGenApp(MDApp):
     """Main App."""
 
     # Data
-    word_state_dict = DictProperty({})
-    """:class:`~kivy.properties.DictProperty` containing all words that could not be processed by Queue."""
     current_template_name = StringProperty("Portuguese Vocab")
     template = ObjectProperty()
+    # CONFIG
     apkg_export_dir = ConfigParserProperty("apgks", "Paths", "apkg_export_dir", "app")
     anki_template_dir = ConfigParserProperty(
         "vocab_card", "Paths", "anki_template_dir", "app"
     )
+    primary_palette = ConfigParserProperty("Red", "Theme", "primary_palette", "app")
+    accent_palette = ConfigParserProperty("Amber", "Theme", "accent_palette", "app")
+    theme_style = ConfigParserProperty("Light", "Theme", "theme_style", "app")
+
     busy = BooleanProperty(False)
     busy_modal = ObjectProperty(None)  #
 
@@ -54,52 +54,37 @@ class AnkiCardGenApp(MDApp):
 
     def build_config(self, config):  # pylint: disable=no-self-use
         """If no config-file exists, sets the default."""
-        config.setdefaults(
-            "Theme",
-            {
-                "primary_palette": "Red",
-                "accent_palette": "Amber",
-                "theme_style": "Light",
-            },
-        )
-        config.setdefaults(
-            "Paths", {"anki_template_dir": "vocab_card", "apkg_export_dir": "apkgs"},
-        )
-        # os.makedirs("../app_data/", exist_ok=True)
-        # os.makedirs("../app_data/apkgs/", exist_ok=True)
+        config.setdefaults("Theme", {})
+        config.setdefaults("Paths", {})
+
+    def bind_theme_cls_and_config(self):
+        """Bind :attr:`theme_cls` and the corresponding :class:`~kivy.properties.ConfigParserProperties`."""
+        keys = self.config["Theme"]
+        self.bind(**{key: self.theme_cls.setter(key) for key in keys})
+        self.theme_cls.bind(**{key: self.setter(key) for key in keys})
+        for key in keys:
+            setattr(self.theme_cls, key, getattr(self, key))
 
     def build(self):
         """Set up App and return :class:`custom_widgets.MainMenu` as root widget."""
-        # Config and Theme
         add_missing_templates()
-        self.theme_cls = ThemeManager(  # pylint: disable=attribute-defined-outside-init
-            **self.config["Theme"]
-        )
-        self.word_state_dict = self.get_word_states()
-        print(self.word_state_dict)
+        self.bind_theme_cls_and_config()
         return MainMenu()
-
-    def save_theme(self, *_):
-        """Save current theme to config file."""
-        for key in self.config["Theme"]:
-            self.config["Theme"][key] = getattr(self.theme_cls, key)
-        self.config.write()
-
-    def load_by_config_key(self, key):
-        """Use :func:`utils.smart_loader` to load attribute from path in :attr:`config` [attribute]."""
-        path = self.config["Paths"][key]
-        setattr(self, key, smart_loader(path))
-
-    def save_by_config_key(self, key, *_, obj=None):
-        """Use :func:`utils.smart_saver` to save current attribute to path in :attr:`config` [attribute]."""
-        if obj is None:
-            obj = getattr(self, key)
-        path = self.config["Paths"][key]
-        smart_saver(obj, path)
 
     def get_current_template_db(self):
         """Return data-base object for :attr:`current_template_name`."""
         return get_template(self.current_template_name)
+
+    def get_word_states(self):
+        """Return dict of word-states for current template from data-base."""
+        with db_session:
+            return {
+                card.name: card.state for card in self.get_current_template_db().cards
+            }
+
+    word_state_dict = AliasProperty(
+        getter=get_word_states, setter=None, bind=["current_template_name"]
+    )
 
     def setup_template(self):
         """Initialize :class:`templates.Template` and adds it to the single_word screen."""
@@ -115,14 +100,6 @@ class AnkiCardGenApp(MDApp):
         """Set up template on start of app."""
         super(AnkiCardGenApp, self).on_start()
         self.setup_template()
-        print(vars(self.config))
-
-    def get_word_states(self):
-        """Return dict of word-states for current template from data-base."""
-        with db_session:
-            return {
-                card.name: card.state for card in self.get_current_template_db().cards
-            }
 
     @mainthread
     def on_busy(self, *_):
