82f35ff603d4eefb64fff41b33cce196679f6831
David Fischer <d_fi@gmx.de>
2020-09-01 14:08:51 +0200

    update: directly render README.md
    Former-commit-id: d3d3eb5916afef0edc831f730105f7d97af67c70
    Former-commit-id: 74648bd18f5388f3327c4d36d4aa73c83f5149aa

--- .pre-commit/render_readme.py
+++ .pre-commit/render_readme.py
@@ -1,40 +1,67 @@
 #!/usr/bin/env python3
-"""Script to render the README jinja template."""
+"""Script to render jinja-blocks in ``README.md``."""
 import glob
 import os
+import re
 
 import jinja2
 
 
-def main(path, overwrite=False):
-    """Main function."""
-    with open(path, "r") as file:
-        template_string = file.read()
+JINJA_BLOCK_REGEX = r"<!-- jinja-block (\w*)(.*)jinja-block \1-->"
+JINJA_OUT_REGEX = r"<!-- jinja-out %s.*-->"
 
+
+def get_jinja_out_regex(block_name):
+    return (
+        rf"(?<=<!-- jinja-out {block_name} start-->)"
+        r".*"
+        rf"(?=<!-- jinja-out {block_name} end-->)"
+    )
+
+
+def get_jinja_blocks(string):
+    return re.findall(JINJA_BLOCK_REGEX, string, flags=re.DOTALL)
+
+
+def render_single_block(block, **kwargs):
     template = jinja2.Template(
-        template_string,
+        block,
         line_statement_prefix="$",
         line_comment_prefix="$$",
         trim_blocks=True,
         lstrip_blocks=True,
         keep_trailing_newline=True,
     )
+    return template.render(**kwargs)
 
+
+def refresh_single_output(file_string, block_name, replacement):
+    pattern = get_jinja_out_regex(block_name)
+    return re.sub(pattern, replacement, file_string, flags=re.DOTALL)
+
+
+def get_render_kwargs():
     img_files = sorted(glob.glob("screenshots/*.png"))
     words = [
         {"name": name.split("/")[-2], "sides": sorted(glob.glob(f"{name}*.png"))}
         for name in sorted(glob.glob("screenshots/*/"))
     ]
-    rendered = template.render(words=words, img_files=img_files)
-
-    # remove ".pre-commit/" and ".jinja" extension from path
-    out_path = os.path.basename(os.path.splitext(path)[0])
-    if not os.path.exists(out_path) or overwrite:
-        with open(out_path, "w") as file:
-            file.write(rendered)
-    else:
-        raise os.error("File exists. To overwrite, set -o flag.")
+    comment_tag = "<!-- -->"
+    return {**locals()}
 
 
-for file in glob.glob(".pre-commit/*.jinja"):
-    main(file, overwrite=True)
+def new_main():
+    file_name = "README.md"
+    with open(file_name, "r") as file:
+        file_string = file.read()
+
+    for name, block in get_jinja_blocks(file_string):
+        render_kwargs = get_render_kwargs()
+        out_block = render_single_block(block, **render_kwargs)
+        file_string = refresh_single_output(file_string, name, out_block)
+    with open(file_name, "w") as file:
+        file.write(file_string)
+
+
+if __name__ == "__main__":
+    new_main()
