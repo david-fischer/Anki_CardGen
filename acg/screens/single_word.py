869ea6ee75e2dbc8b640aae5ff725e77b8f30e8b
David Fischer <d_fi@gmx.de>
2020-09-13 13:42:07 +0200

    add: actions to single-word screen
    Former-commit-id: 18bbb19a715345dd27b4a988ceef331734354227

--- acg/screens/single_word.py
+++ acg/screens/single_word.py
@@ -3,6 +3,8 @@
 from kivy.properties import ObjectProperty
 from kivy.uix.floatlayout import FloatLayout
 
+from utils import set_screen
+
 
 class SingleWordRoot(FloatLayout):
     """Root widget of the single_word screen."""
@@ -16,3 +18,19 @@ class SingleWordRoot(FloatLayout):
         if self.template.parent:
             self.template.parent.remove_widget(self.template)
         self.scroll_view.add_widget(self.template)
+
+    @staticmethod
+    def back_to_queue(*_):
+        """Set screen to ``"queue"``."""
+        set_screen("queue")
+
+    def proceed(self, *_):
+        """Accept data and set screen to ``"queue"``."""
+        self.template.get_results()
+        set_screen("queue")
+
+    def refresh(self, *_):
+        """Crawl data and update database."""
+        self.template.set_data_from_parsers()
+        self.template.update_fields()
+        self.template.save_base_data_to_db()
