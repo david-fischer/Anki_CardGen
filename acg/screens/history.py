408b0c4acdc70cd4ed76751a8d24eb86d96ce661
David Fischer <d_fi@gmx.de>
2020-10-17 16:28:46 +0200

    add: dropdown menu on click on items
    * delete: delete word from history
    * back to queue: move word back to queue
    * edit: edit word and move back to queue
    
    
    Former-commit-id: 3c024672acb1baa10c9fce596cd57171d79513bd

--- acg/screens/history.py
+++ acg/screens/history.py
@@ -7,10 +7,12 @@ from kivy.properties import ObjectProperty
 from kivy.uix.floatlayout import FloatLayout
 from kivymd.app import MDApp
 from kivymd.toast import toast
+from kivymd.uix.menu import MDDropdownMenu
 from pony.orm import db_session
 
+from custom_widgets.dialogs import TextInputDialog
 from db import export_cards
-from utils import not_implemented_toast
+from utils import app_busy, not_implemented_toast, set_word_state, widget_by_id
 
 
 class HistoryRoot(FloatLayout):
@@ -31,6 +33,16 @@ class HistoryRoot(FloatLayout):
 
     def __post_init__(self, *_):
         self.speed_dial.icon = "content-save"
+        self.dropdown_menu = MDDropdownMenu(
+            caller=self.ids.history_list,
+            position="center",
+            width_mult=4,
+            items=[{"text": "back to queue"}, {"text": "delete"}, {"text": "edit"},],
+        )
+        self.dropdown_menu.on_release = self.on_dropdown_item
+        self.text_input_dialog = TextInputDialog(title="Edit Word:")
+        self.text_input_dialog.callback = self.text_input_dialog_callback
+        # self.dropdown_menu.bind(on_release=self.on_dropdown_item)
 
     def export_cards(self, button):
         """Depending on which button was pressed, exports one of the following.
@@ -72,3 +84,47 @@ class HistoryRoot(FloatLayout):
     def sort(*_):
         """Placeholder-function."""
         not_implemented_toast()
+
+    def click_on_item(self, item, *_):
+        """Open dropdown menu with ``item`` as caller."""
+        self.dropdown_menu.caller = item
+        self.dropdown_menu.open()
+        print(item.text)
+
+    def on_dropdown_item(self, item, *_):
+        """Call option corresponding to clicked item."""
+        caller = self.dropdown_menu.caller
+        if item.text == "delete":
+            self.delete_item(caller.text)
+        elif item.text == "edit":
+            self.edit_item(caller.text)
+        else:
+            self.move_back_to_queue(caller.text)
+        self.dropdown_menu.dismiss()
+
+    @staticmethod
+    def delete_item(item):
+        """Delete ``text`` from the current template in the data-base and from ``app.word_state_dict``."""
+        app = MDApp.get_running_app()
+        del app.word_state_dict[item]
+        with db_session:
+            app.get_current_template_db().get_card(item).delete()
+
+    @app_busy
+    def edit_item(self, item):
+        """Open :attr:`text_input_dialog` to edit the word."""
+        self.text_input_dialog.default_text = item
+        self.text_input_dialog.open()
+
+    @staticmethod
+    def move_back_to_queue(item):
+        """Move item back to queue."""
+        old_state = MDApp.get_running_app().word_state_dict[item]
+        new_state = "waiting" if old_state == "error" else "ready"
+        set_word_state(item, new_state)
+
+    def text_input_dialog_callback(self, button_txt, text):
+        """If the ``OK``-button is pressed, delete old entry and add edited entry to queue."""
+        if button_txt == "OK":
+            self.delete_item(self.text_input_dialog.default_text)
+            widget_by_id("queue").add_waiting(text)
