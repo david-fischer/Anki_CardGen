77b859eb50b8939fa5a619538fdb7108aef2f96c
David Fischer <d_fi@gmx.de>
2020-12-19 20:00:24 +0100

    stuff
    Former-commit-id: 2c7b956e6a599334ed4e8ac1d3eddf65d15252ca

--- acg/unused_utils.py
+++ acg/unused_utils.py
@@ -28,3 +28,35 @@ def route_calls_to_member(member, calls):
         return cls
 
     return decorator_func
+
+
+#
+# @attr.s(auto_attribs=True)
+# class OpenDialogMixin:
+#     content_cls_name: str = "CustomContentBase"
+#     dialog_cls_name: str = "CustomDialog"
+#     dialog_title: str = "Default Title"
+#     _dialog: MDDialog = None
+#
+#     def _init_dialog(self):
+#         content_cls = Factory.get(self.content_cls_name)
+#         dialog_cls = Factory.get(self.dialog_cls_name)
+#         print(self.dialog_cls_name, self.content_cls_name)
+#         print(dialog_cls, content_cls)
+#         self._dialog = dialog_cls(
+#             title=self.dialog_title,
+#             content_cls=content_cls(),
+#             callback=self.dialog_callback,
+#         )
+#
+#     def dialog_callback(self, button, result):
+#         """Callback for dialog."""
+#
+#     def show_dialog(self, raw_dialog_data):
+#         if self._dialog is None:
+#             self._init_dialog()
+#         self._dialog.content_cls.data = self.process_dialog_data(raw_dialog_data)
+#         self._dialog.open()
+#
+#     def process_dialog_data(self, raw_dialog_data):
+#         return raw_dialog_data
