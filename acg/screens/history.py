c645853678bd8cedadbf31c89fd20622252eff34
David Fischer <d_fi@gmx.de>
2021-01-01 00:27:58 +0100

    refactor: move exporter to separate file
    Former-commit-id: 86350f9436d333a7b4b07196e25498a7c05ed859

--- acg/screens/history.py
+++ acg/screens/history.py
@@ -1,34 +1,26 @@
 """Implements :class:`HistoryMain`, the root widget of the history screen."""
 
-import os
-
-from bidict import bidict
 from kivy.clock import Clock
-from kivy.properties import ObjectProperty
+from kivy.properties import DictProperty, ObjectProperty
 from kivy.uix.floatlayout import FloatLayout
 from kivymd.app import MDApp
-from kivymd.toast import toast
 from kivymd.uix.menu import MDDropdownMenu
 from pony.orm import db_session
 
 from ..custom_widgets.dialogs import TextInputDialog
-from ..db import export_cards
+from ..exporter import export_cookbook
 from ..utils import app_busy, not_implemented_toast, set_word_state, widget_by_id
 
 
 class HistoryRoot(FloatLayout):
     """Root widget of the history screen."""
 
-    action_button_data = bidict(
-        {
-            "new-box": "export new cards",
-            "content-save-all": "export all cards",
-            "check-box-multiple-outline": "select specific cards to export",
-        }
-    )
+    speed_dial_buttons = DictProperty()
     speed_dial = ObjectProperty()
+    export_cookbook = export_cookbook
 
     def __init__(self, **kwargs):
+        self.speed_dial_buttons = self.export_cookbook.to_button_dict()
         super().__init__(**kwargs)
         Clock.schedule_once(self.__post_init__)
 
@@ -48,37 +40,6 @@ class HistoryRoot(FloatLayout):
         self.text_input_dialog = TextInputDialog(title="Edit Word:")
         self.text_input_dialog.callback = self.text_input_dialog_callback
         # self.dropdown_menu.bind(on_release=self.on_dropdown_item)
-
-    def export_cards(self, button):
-        """Depending on which button was pressed, exports one of the following.
-
-            * all cards
-            * all new cards (i.e. cards that have been generated but not exported)
-            * custom selection of cards (NOT YET IMPLEMENTED)
-
-
-        TODO: IMPLEMENT
-        """
-        self.speed_dial.close_stack()
-        option = self.action_button_data[button.icon]
-        if option == "export new cards":
-            states = ["done"]
-        elif option == "export all cards":
-            states = ["done", "exported"]
-        else:
-            toast("NOT IMPLEMENTED YET.", duration=10)
-            return False
-        with db_session:
-            app = MDApp.get_running_app()
-            current_template = app.get_current_template_db()
-            cards = current_template.get_cards_by_selector(lambda c: c.state in states)
-            if not cards:
-                toast("Empty Selection. Could not export cards.")
-                return False
-            print(os.path.exists(app.apkg_export_dir))
-            export_cards(cards, app.apkg_export_dir, app.get_anki_template_dir())
-        toast(f"Exported cards to {app.apkg_export_dir}.", duration=5)
-        return True
 
     @staticmethod
     def filter(*_):
