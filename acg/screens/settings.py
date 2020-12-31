667f440cfc495ba929eaba35e01d61f3246879c5
David Fischer <d_fi@gmx.de>
2020-12-31 14:56:12 +0100

    update: use custom_widgets.settings.py for settings screen
    Former-commit-id: 742bd3cf06a8bbefb09177ced8521139ca9142b8

--- acg/screens/settings.py
+++ acg/screens/settings.py
@@ -1,56 +1,21 @@
 """Implements :class:`SettingsRoot`, the root widget for the settings screen."""
-import os
-from time import sleep
+from ..custom_widgets import PathSection, SectionBase, SettingsWidget, ThemeSection
+from ..custom_widgets.behaviors import ChildrenFromDataBehavior
+from ..design_patterns.factory import CookBook
 
-from kivy.properties import ObjectProperty
-from kivy.uix.boxlayout import BoxLayout
-from kivymd.app import MDApp
-from kivymd.uix.picker import MDThemePicker
-from kivymd.uix.snackbar import Snackbar
-
-from ..utils import app_busy
+section_cookbook = CookBook()
+section_cookbook.register("Theme")(ThemeSection)
+section_cookbook.register("Paths")(PathSection)
 
 
-class SettingsRoot(BoxLayout):
+@section_cookbook.register("Template")
+class TemplateSection(SectionBase, ChildrenFromDataBehavior):
+    """Not implemented yet."""
+
+    # TODO: implement
+
+
+class SettingsRoot(SettingsWidget):
     """Root widget of the settings screen."""
 
-    theme_dialog = ObjectProperty()
-    snackbar = ObjectProperty()
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.theme_dialog = MDThemePicker()
-        self.snackbar = Snackbar()
-
-    @staticmethod
-    @app_busy
-    def sleep_some():
-        """Test-function."""
-        sleep(5)
-
-    def check_dir(self, directory):
-        """
-        Check if ``directory`` exists, offer user choice to create it if not.
-
-        Then, set apkg_export_dir in app and the text-field accordingly.
-        """
-        print(directory)
-        if os.path.exists(directory):
-            self.set_export_dir(directory)
-            return
-        self.snackbar = Snackbar(
-            text=f"{directory}   does not exists. Create?",
-            button_text="CREATE",
-            button_callback=lambda *_: self.set_export_dir(directory),
-            button_color=MDApp.get_running_app().theme_cls.primary_color,
-            duration=10,
-        )
-        self.snackbar.show()
-        self.ids.export_dir_text_field.text = MDApp.get_running_app().apkg_export_dir
-
-    def set_export_dir(self, directory):
-        """Set new export dir to value ``directory``."""
-        self.snackbar.duration = 0
-        os.makedirs(directory, exist_ok=True)
-        MDApp.get_running_app().apkg_export_dir = directory
-        self.ids.export_dir_text_field.text = directory
+    cookbook = section_cookbook
